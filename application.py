0747080eaac1c7883e4fba1e2c31c3a98e346b31
Mr Hanzla <mr.hanzla101@gmail.com>
2022-07-21 16:34:12 +0500

    change secret key & add routes for tic tac toe

--- application.py
+++ application.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, session
+from flask import Flask, redirect, render_template, session, url_for
 import os
 
 app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY='dev'
-)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+def show(msg=''):
+    print('*'*40)
+    print(msg)
+    print('*'*40)
+
+def toggle_turn():
+    turn = session['turn']
+    session['turn'] = 'O' if turn=='X' else 'X'
+
 
 @app.route('/')
 def index():
@@ -12,11 +20,27 @@ def index():
 
 @app.route('/ttt')
 def ttt():
-    session.clear()
     if 'board' not in session:
         session['board'] = [[None for _ in range(3)] for _ in range(3)]
-    print(f"Board: {session['board']}")
-    return render_template('ttt.html')
+        session['turn'] = 'X'
+    show(f" {session['board']}")
+    return render_template('ttt.html', game={
+        'board': session['board'],
+        'turn': session['turn']
+    })
+
+@app.route('/move/<int:row>/<int:col>')
+def move(row, col):
+    turn = session['turn']
+    show(f"Row: {row}, Col: {col}, Turn: {turn}")
+    session['board'][row][col] = turn
+    toggle_turn()
+    return redirect(url_for('ttt'))
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect(url_for('ttt'))
 
 if __name__ == '__main__':
     app.run(debug=True)
